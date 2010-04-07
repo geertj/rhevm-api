ef33e4967f59463b2efab5b9ef7ce2c24264bc27
Geert Jansen <geertj@gmail.com>
2010-04-07 02:32:48 +0200

    VMs are identified by their GUID.

--- lib/rhevm/module/nic.py
+++ lib/rhevm/module/nic.py
@@ -21,7 +21,7 @@ class NicCollection(RhevmCollection):
     objectname = 'nic'
 
     def show(self, vm, id):
-        filter = create_filter(name=vm)
+        filter = create_filter(vmid=vm)
         result = powershell.execute('Select-Vm | %s'
                                     ' | Tee-Object -Variable vm' % filter)
         if len(result) != 1:
@@ -33,7 +33,7 @@ class NicCollection(RhevmCollection):
         return result[0]
 
     def list(self, vm, **args):
-        filter = create_filter(name=vm)
+        filter = create_filter(vmid=vm)
         result = powershell.execute('Select-Vm | %s'
                                     ' | Tee-Object -Variable vm' % filter)
         if len(result) != 1:
@@ -43,7 +43,7 @@ class NicCollection(RhevmCollection):
         return result
 
     def create(self, vm, input):
-        filter = create_filter(name=vm)
+        filter = create_filter(vmid=vm)
         result = powershell.execute('Select-Vm | %s'
                                     ' | Tee-Object -Variable vm' % filter)
         if len(result) != 1:
@@ -60,7 +60,7 @@ class NicCollection(RhevmCollection):
         return url, result[0]
 
     def delete(self, vm, id):
-        filter = create_filter(name=vm)
+        filter = create_filter(vmid=vm)
         result = powershell.execute('Select-Vm | %s'
                                     ' | Tee-Object -Variable vm' % filter)
         if len(result) != 1:
