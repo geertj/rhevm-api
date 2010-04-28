35fdaad9e64ed306cf92a3ec4169f66f2923ae56
Geert Jansen <geertj@gmail.com>
2010-04-28 22:01:59 +0200

    The tag changes require argproc 1.2

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
     entry_points = { 'console_scripts': [
             'rhevm-api-cmdline = rhevm.server:cmdline',
             'rhevm-api-isapi = rhevm.server:isapi'] },
-    install_requires = ['argproc >= 1.1', 'winpexpect >= 1.2',
+    install_requires = ['argproc >= 1.2', 'winpexpect >= 1.2',
                         'python-rest >= 1.2', 'pyyaml >= 3.09',
                         'isapi_wsgi >= 0.4.1'],
     cmdclass = { 'build': mybuild, 'gentab': gentab },
