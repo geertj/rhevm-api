f4cec42e6a60aa1f1b2244ca91377ee7c61cbffe
Geert Jansen <geertj@gmail.com>
2010-06-08 21:59:11 +0200

    Make tags work again.

--- lib/rhevm/collection.py
+++ lib/rhevm/collection.py
@@ -7,8 +7,15 @@
 # "AUTHORS" for a complete overview.
 
 from rest.collection import Collection
+from rhevm.api import powershell
 
 
 class RhevmCollection(Collection):
+    """Base class for all rhevm-api collections."""
 
     objectname = None
+
+    def _get_tags(self):
+        tags = super(RhevmCollection, self)._get_tags()
+        tags.append('rhevm%d%s' % powershell.version[:2])
+        return tags
